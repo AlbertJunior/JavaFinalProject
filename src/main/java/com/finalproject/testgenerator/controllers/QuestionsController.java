d64229c1c9e8a4946844bbe7ed405c20be6b3a03
Ramona Albert <albert.ramona99@gmail.com>
2020-05-12 12:22:03 +0300

    initial commit

---
+++ src/main/java/com/finalproject/testgenerator/controllers/QuestionsController.java
@@ -0,0 +1,13 @@
+package com.finalproject.testgenerator.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuestionsController {
+
+    @GetMapping("/")
+    String hello(){
+        return "Hello wordl!";
+    }
+}
---
+++ src/test/java/com/finalproject/testgenerator/TestgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.testgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
